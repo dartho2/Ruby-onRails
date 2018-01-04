4267a8d94153f9bd351d36bde906d7368cc898d4
Jan Nowak <jannowak@example.com>
2018-01-04 11:34:51 +0100

    finished panel Admin

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Admin::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
+  before_action :is_admin?, only: [:new, :create]
 
   protected
 
@@ -8,4 +9,15 @@ class Admin::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 
+  def is_admin?
+    if admin_signed_in? # if user signed
+      if current_admin.admin? # if adminstrator return true
+        true
+      else
+        redirect_to some_path
+      end
+    else
+      redirect_to admin_session_path
+    end
+  end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
