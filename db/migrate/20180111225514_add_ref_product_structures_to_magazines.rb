e477f17ab755c7158d32be760f148d42abfe19d0
Jan Nowak <jannowak@example.com>
2018-01-13 23:01:19 +0100

    heroku 2

--- db/migrate/20180111225514_add_ref_product_structures_to_magazines.rb
+++ db/migrate/20180111225514_add_ref_product_structures_to_magazines.rb
@@ -1,6 +1,6 @@
 class AddRefProductStructuresToMagazines < ActiveRecord::Migration[5.1]
   def change
-    add_reference :product_structures, :magazine, foreign_key: true
+    add_references :product_structures, :magazine, foreign_key: true
 
   end
 end
